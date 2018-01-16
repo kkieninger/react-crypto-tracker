048462487348713909f63b46c78b878591bc8491
ktkienin@gmail.com <ktkienin@gmail.com>
2018-01-15 23:39:43 -0500

    small tweaks to footer styling

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css';
 const Footer = () => {
   return (
     <div className="Footer black mt5 pa2 relative">
-      <p className="copyright center tc ">a project by <a className="no-underline" href="//kkieninger.com/">Kevin Kieninger</a></p>
+      <p className="copyright center tc fw2">a project by <a className="no-underline underline-hover" href="//kkieninger.com/">Kevin Kieninger</a></p>
     </div>
   );
 }
